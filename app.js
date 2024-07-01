fbe2f76449f8fad9c660dc223395b883baf397d4
kumarabhishekbansal <kumarabhishekbansal>
2024-07-01 11:33:00 +0530

    build updates

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["https://abhishek-foodbite.onrender.com/","http://localhost:3000"],
     methods: ["GET", "POST"],
   })
 );
